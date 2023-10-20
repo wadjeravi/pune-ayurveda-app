59f99993587279eb7fad83557d7f4338ccbb8e6a
SakshiPensalwar9 <sakshipensalwar@gmail.com>
2023-10-21 02:15:08 +0530

    Changes in Carosuel

--- components/home/carosuelp/Carosuel.jsx
+++ components/home/carosuelp/Carosuel.jsx
@@ -1,51 +1,73 @@
-import React, { useState, useRef } from 'react';
-import { View, Image, TouchableOpacity } from 'react-native';
-import Swiper from 'react-native-swiper';
-import styles from './Carosuel.style'
+import React from 'react';
+import { View, StyleSheet ,Text,Image} from 'react-native';
+//import {  colors } from '../../../constants'
+import Swiper from 'react-native-swiper'
 
-const Carousel = () => {
-  const images = [
-    "https://www.unitedayurveda.in/wp-content/uploads/2022/10/banner1-1.jpg",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1geB-fp8l5IlOheox0PEB4K4DOqupOA0ysA&usqp=CAU",
-    "https://images.hindustantimes.com/rf/image_size_630x354/HT/p2/2019/09/06/Pictures/ayurveda_80d7f144-d08d-11e9-a264-bc92e50e5c68.jpg"
-  ];
+const OfferSlider = () => {
+    return (
+        <View>
+            <View style={styles.offerSlider}>
+                <Swiper autoplay={true} autoplayTimeout={5} showsButtons={false}
+                    dotColor="grey" activeDotColor="black"   
+               >
+                  <View style={styles.slide}>
+                       <Image source={require('../../../assets/images/slider1.jpg')} style={styles.image} />
+                       </View>
+                  <View style={styles.slide}>
+                       <Image source ={require('../../../assets/images/slider2.jpg')} style={styles.image} />
+                       </View>
+                  <View style={styles.slide}>
+                       <Image source={require('../../../assets/images/slider3.jpg')} style={styles.image}/>
+                       </View>
+                </Swiper>
+            </View>
+        </View>
+    )
+}
+export default OfferSlider
+const styles = StyleSheet.create({
+    offerSlider: {
+        width: '90',
+        height: 200,
+        paddingHorizontal: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom:40,
+        // marginVertical: 10,
+    },
+    slide: {
+        width: '100',
+        height: 200,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    image: {
+        width: 300,
+        height: 150,
+        borderRadius: 30,
+        resizeMode:'cover',
+        padding:50,
+        marginBottom:35,
+    },
 
-  const [currentIndex, setCurrentIndex] = useState(0);
-  let swiperRef = useRef(null);
+    dot: {
+        backgroundColor: 'grey',
+        width: 10, 
+        height: 10, 
+        borderRadius: 5, 
+        margin: 5,
+    },
+    activeDot: {
+        backgroundColor: 'black', 
+        width: 10, 
+        height: 10,
+        borderRadius: 5, 
+        margin: 5,
+    },
+})
 
-  const scrollToIndex = (index) => {
-    setCurrentIndex(index);
-    swiperRef.current?.scrollTo(index);
-  };
 
-  return (
-    <View style={styles.container}>
-      <Swiper
-        ref={swiperRef}
-        style={styles.slider}
-        showsPagination={false}
-        onIndexChanged={(index) => setCurrentIndex(index)} 
-      >
-        {images.map((_, index) => (
-          <View style={styles.slide} key={index}>
-            <Image style={styles.image} source={{ uri: images[index] }} />
-          </View>
-        ))}
-      </Swiper>
 
-      <View style={styles.dotView}>
-        {images.map((_, index) => (
-          <TouchableOpacity
-            key={index.toString()}
-            style={[
-              styles.circle,
-              { backgroundColor: index === currentIndex ? 'black' : 'grey' },
-            ]}
-            onPress={() => scrollToIndex(index)}
-          />
-        ))}
-      </View>
-    </View>
-  );
-};
-export default Carousel;
+
+
+   
